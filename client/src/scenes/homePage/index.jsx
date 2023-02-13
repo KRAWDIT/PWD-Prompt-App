2404724c8587cad69f2bb068b47eb4f41726e0d1
lazy-cell <116555621+lazy-cell@users.noreply.github.com>
2023-02-13 22:59:26 +0700

    Initial Commit

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,44 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "scenes/navbar";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import UserWidget from "scenes/widgets/UserWidget";
+
+const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
+  return (
+    <Box>
+      <Navbar style={{ height: "50px" }} />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+        style={{ marginTop: "50px" }}
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}></Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <Box m="2rem 0" />
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default HomePage;
